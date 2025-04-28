fd2cc6876221fa2c220a96d4f04d72ddf841a9c9
Ricardo Beale <139693122+RiccoWrld@users.noreply.github.com>
2025-04-28 13:19:06 -0400

    Update chatclient.py
    removed commentss

--- chatclient.py
+++ chatclient.py
@@ -1,14 +1,14 @@
 import socket
 import threading
 
-# Change this to the server's IP address if you're running this on a different machine
-HOST = '127.0.0.1'  # Use the server's public IP if you're connecting from another machine
+
+HOST = '127.0.0.1'  
 PORT = 55555
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-# Function to receive messages
+
 def receive():
     while True:
         try:
@@ -23,19 +23,19 @@ def receive():
             client.close()
             break
 
-# Function to send messages
+
 def send():
     while True:
         message = input()
         
         if message.lower() == '!exit':
-            client.send('!exit'.encode('ascii'))  # Notify the server you're leaving
-            client.close()  # Close the client socket
-            break  # Exit the loop
+            client.send('!exit'.encode('ascii'))  
+            client.close()  
+            break 
 
         client.send(message.encode('ascii'))
 
-# Start receiving and sending messages concurrently using threads
+
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
